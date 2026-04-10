946f9c91e00c1624794833b5af0e326b4f19f115
ocavue <ocavue@users.noreply.github.com>
2026-04-10 16:56:01 +1000

    feat: split twoslash-content into twoslash-positioner and twoslash-popup (#23)

--- src/elements.ts
+++ src/elements.ts
@@ -1,29 +1,58 @@
 import { defineCustomElement, registerCustomElement } from '@aria-ui/core'
 import {
-  tooltipContentEvents,
-  tooltipContentProps,
-  TooltipRootElement,
-  TooltipTriggerElement,
-  useTooltipContent,
-  type TooltipContentEvents,
-  type TooltipContentProps,
-} from '@aria-ui/tooltip/elements'
+  setupTooltipPopup,
+  setupTooltipPositioner,
+  setupTooltipRoot,
+  setupTooltipTrigger,
+  TooltipPopupPropsDeclaration,
+  TooltipPositionerPropsDeclaration,
+  TooltipRootPropsDeclaration,
+  TooltipTriggerPropsDeclaration,
+} from '@aria-ui/elements/tooltip'
 
-class TwoslashContentElement extends defineCustomElement<
-  TooltipContentProps,
-  TooltipContentEvents
->({
-  props: {
-    ...tooltipContentProps,
-    placement: { default: 'bottom' },
-    rootBoundary: { default: 'document' },
+class TwoslashPositionerElement extends defineCustomElement(
+  setupTooltipPositioner,
+  {
+    ...TooltipPositionerPropsDeclaration,
+    placement: {
+      ...TooltipPositionerPropsDeclaration.placement,
+      default: 'bottom',
+    },
+    rootBoundary: {
+      ...TooltipPositionerPropsDeclaration.rootBoundary,
+      default: 'document',
+    },
   },
-  events: tooltipContentEvents,
-  setup: useTooltipContent,
-}) {}
+) {}
 
-export function register() {
-  registerCustomElement('twoslash-content', TwoslashContentElement)
-  registerCustomElement('twoslash-root', TooltipRootElement)
-  registerCustomElement('twoslash-trigger', TooltipTriggerElement)
+class TwoslashRootElement extends defineCustomElement(
+  setupTooltipRoot,
+  {
+    ...TooltipRootPropsDeclaration
+  }
+) {} 
+
+class TwoslashTriggerElement extends defineCustomElement(
+  setupTooltipTrigger,
+  {
+    ...TooltipTriggerPropsDeclaration,
+    openDelay: {
+      ...TooltipTriggerPropsDeclaration.openDelay,
+      default: 100,
+    },
+  }
+) {}
+
+class TwoslashPopupElement extends defineCustomElement(
+  setupTooltipPopup,
+  {
+    ...TooltipPopupPropsDeclaration
+  }
+) {}
+
+export function register(): void  {
+  registerCustomElement('twoslash-positioner', TwoslashPositionerElement)
+  registerCustomElement('twoslash-popup', TwoslashPopupElement)
+  registerCustomElement('twoslash-root', TwoslashRootElement)
+  registerCustomElement('twoslash-trigger', TwoslashTriggerElement)
 }
